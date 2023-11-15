ff292c6a7eb8ca3bfb04772cb2f226ae4aa22fa6
Selahattin Dulgeroglu <smdulgeroglu@hotmail.com>
2023-11-15 21:44:35 +0300

    ChartValue exposed to outside module

--- Sources/SwiftUICharts/Base/Chart/ChartValue.swift
+++ Sources/SwiftUICharts/Base/Chart/ChartValue.swift
@@ -3,8 +3,8 @@ import SwiftUI
 /// Representation of a single data point in a chart that is being observed
 @available(iOS 13.0, *)
 public class ChartValue: ObservableObject {
-    @Published var currentString: String = ""
-    @Published var currentValue: Double = 0
-    @Published var currentValueTarget: Double = 0
-    @Published var interactionInProgress: Bool = false
+    @Published public var currentString: String = ""
+    @Published public var currentValue: Double = 0
+    @Published public var currentValueTarget: Double = 0
+    @Published public var interactionInProgress: Bool = false
 }
